7eca755a64fbbe190616dcac14bb87cfe48b997d
DASE1312 <67829871+DASE1312@users.noreply.github.com>
2020-07-16 17:05:05 -0500

    splash

--- app/src/main/java/com/example/retrofit2/SplashActivity.kt
+++ app/src/main/java/com/example/retrofit2/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.retrofit2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIEMPO:Long=3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            },SPLASH_TIEMPO)
     }
 }
